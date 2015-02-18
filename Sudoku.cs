6b0248da88c5aa732eaeb6591bd0c0814241a290
vot24100 <mladen.m.savov@gmail.com>
2015-02-18 17:06:11 +0200

    1. Adding first version of Sudoku.cs containing ReadSudoku method which fills two arrays from a text file, and PrintSudoku method which prints the arrays for verification. 2. Adding two text files sudoku1.txt and sudoku2.txt containing unsolved and solved Sudoku games

---
+++ Sudoku.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+namespace Sudoku
+{
+    class Sudoku
+    {
+        // Arrays with Sudoku - unsolved and solved
+        static int[,] sudokuTask = new int[9, 9];
+        static int[,] sudokuSolved = new int[9, 9];
+
+        static void ReadSudoku()
+        {
+            // Using Random to create a random number corresponding to a file with Sudoku
+            Random rand = new Random();
+            int fileNumber = rand.Next(1, 3);
+
+            // Using StringBuilder to create a filePath to the file with Sudoku
+            StringBuilder filePath = new StringBuilder();
+            filePath.Append(@"..\..\sudoku");
+            filePath.Append(fileNumber);
+            filePath.Append(".txt");
+
+            // Using StreamReader to read Sudoku from the randomly selected file
+            StreamReader reader = new StreamReader(filePath.ToString());
+            using (reader)
+            {
+            // Fill sudokuTask array from the first 9 rows of the file
+                for (int i = 0; i < 9; i++)
+                {
+                    string line = reader.ReadLine();
+
+                    for (int j = 0; j < 9; j++)
+                    {
+                        if (line[j] == '-')
+                        {
+                            sudokuTask[i, j] = 0;
+                        }
+                        else
+                        {
+                            sudokuTask[i, j] = line[j] - '0';
+                        }
+                    }
+                }
+
+                // Fill sudokuSolved array from the next 9 rows of the file
+                for (int i = 0; i < 9; i++)
+                {
+                    string line = reader.ReadLine();
+
+                    for (int j = 0; j < 9; j++)
+                    {
+                         sudokuSolved[i, j] = line[j] - '0';
+                    }
+                }
+            }
+        }
+
+        static void PrintSudoku(int[,] sudoku)
+        {
+            for (int i = 0; i < 9; i++)
+            {
+                for (int j = 0; j < 9; j++)
+                {
+                    Console.Write("{0}", sudoku[i, j]);
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+        }
+
+        static void Main()
+        {
+            ReadSudoku();
+            PrintSudoku(sudokuTask);
+            PrintSudoku(sudokuSolved);
+        }
+    }
+}
