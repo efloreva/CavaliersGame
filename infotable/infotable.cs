d69b9e3930e854bb730341f7ae67caf8c601b14b
marvasgit <marin.vassilev@.gmail.com>
2015-02-20 10:02:57 +0200

    vertical
    vertical lines added  and title changed to  string

--- infotable/infotable.cs
+++ infotable/infotable.cs
@@ -13,8 +13,8 @@ class infoTable
     {
 
         const int maxProgressBarLength = 40;
-        //const string progressBarElement = "█";
-       int progressBarElement =1;
+        const string progressBarElement = "█";
+       //int progressBarElement =1;
         string start = progressBarElement + "Cavalier's Sudoku" + progressBarElement;
         StringBuilder startSB = new StringBuilder(progressBarElement + "Cavalier's Sudoku" + progressBarElement);
         StringBuilder titleSB = new StringBuilder(progressBarElement + "Cavalier's Sudoku" + progressBarElement);
@@ -31,13 +31,13 @@ class infoTable
 
             if (titleSB.Length > maxProgressBarLength)
             {
-                progressBarElement += 1;
+                //progressBarElement += 1;
                 titleSB = new StringBuilder(start);
-               
-                if (progressBarElement>=10)
-                {
-                    progressBarElement = 1;   
-                }
+
+                //if (progressBarElement >= 10)
+                //{
+                //    progressBarElement = 1;
+                //}
                 //titleSB = startSB;
                 //Console.WriteLine(new string('$', 50));
             }
@@ -75,7 +75,6 @@ class infoTable
         public char symbol;
     }
 
-
     static void Main()
     {
         var startTime = DateTime.Now;
@@ -83,13 +82,20 @@ class infoTable
         var count = 22;
         while (true)
         {
-
-
-
             for (int i = 0; i < Console.WindowWidth; i++) // Score Divider
             {
                 PrintAtPosition(i, 5, '-', ConsoleColor.Gray);
                 PrintAtPosition(i, 0, '-', ConsoleColor.Gray);
+                //vertical left
+                PrintAtPosition(0, 1, '|', ConsoleColor.Gray);
+                PrintAtPosition(0, 2, '|', ConsoleColor.Gray);
+                PrintAtPosition(0, 3, '|', ConsoleColor.Gray);
+                PrintAtPosition(0, 4, '|', ConsoleColor.Gray);
+                //vertical right
+                PrintAtPosition(Console.WindowWidth-1, 1, '|', ConsoleColor.Gray);
+                PrintAtPosition(Console.WindowWidth-1, 2, '|', ConsoleColor.Gray);
+                PrintAtPosition(Console.WindowWidth-1, 3, '|', ConsoleColor.Gray);
+                PrintAtPosition(Console.WindowWidth-1, 4, '|', ConsoleColor.Gray);
             }
             PrintStringAtPosition(5, 1, "Team Cavalier Sudoku: ", ConsoleColor.Green);
 
